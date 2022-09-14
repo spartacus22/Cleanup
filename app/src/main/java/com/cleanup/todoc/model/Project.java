5bba2d70656f0e3f15fe63554d231d7dad214d4f
Mehdi B <mehdi.boureghda@yahoo.fr>
2022-09-14 12:05:37 +0200

    test develop

--- app/src/main/java/com/cleanup/todoc/model/Project.java
+++ app/src/main/java/com/cleanup/todoc/model/Project.java
@@ -4,12 +4,14 @@ package com.cleanup.todoc.model;
 import androidx.annotation.ColorInt;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.room.Entity;
 
 /**
  * <p>Models for project in which tasks are included.</p>
  *
  * @author Gaëtan HERFRAY
  */
+@Entity
 public class Project {
     /**
      * The unique identifier of the project
